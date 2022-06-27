fac82465460f9ba8568ca5c912b60274a3f80d99
LabuPaim <victorpaim80@gmail.com>
2022-06-26 22:37:42 -0300

    att mvc

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const cors = require('cors');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.use(express.json());
+app.use(cors());
+
+const paletas = [
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao: 'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/acai-com-leite-condensado.png',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao: 'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/banana-com-nutella.png',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao: 'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/chocolate-belga.png',
+    preco: 7.0,
+  },
+];
+
+app.get('/paletas/todas-paletas', (req, res) => {
+  res.send(paletas);
+});
+
+app.get('/paletas/paleta/:id', (req, res) => {
+  const param__ID = Number(req.params.id);
+  const escolha__PALETA = paletas.find((paleta) => paleta.id === param__ID);
+  res.send(escolha__PALETA);
 });
 
 app.listen(port, () => {
